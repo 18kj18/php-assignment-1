ad72bd0a8e1e9a9692de1a999c8a884486bbf4ae
18KJerzyk.ACC <18kjerzyk.acc@lwetb.ie>
2023-03-09 11:56:32 +0000

    yleldo

--- submit.php
+++ submit.php
@@ -20,8 +20,8 @@
 					}
 			}
 
-	}
-	
+	}else echo('not');
+	echo ($fields);
 	$servername = "10.140.42.235";
     $username = "KJ";
 	$password = "password";
@@ -33,17 +33,17 @@
 	//Create Database
 	$sql = "CREATE DATABASE IF NOT EXISTS kj_database";
 	if (mysqli_query($connection, $sql)) {
-		echo "Database created succesfully";}
+		echo "Database created succesfully <br/>";}
 
 	else{echo "Error creating database:" . mysqli_error($connection);}
 
-
+																		
 	//Create Table
 	$sql = "CREATE TABLE IF NOT EXISTS details (
 				id INT AUTO_INCREMENT PRIMARY KEY,
 				firstname CHAR(64),
 				lastname CHAR(64),
-				email CHAR(300),
+				email TEXT(300),
 				gender ENUM ('male', 'female','other')DEFAULT 'Other',
 				password CHAR(64),
 				address1 CHAR(64),
@@ -52,7 +52,7 @@
 			)";
 
 	if (mysqli_query($connection, $sql)){
-			echo("Table Created Succesfully");}
+			echo("Table Created Succesfully <br/>");}
 	else {	
 			echo("Table not created <br/>");}
 
@@ -61,14 +61,14 @@
 			echo "Failed to connect to mySQL: " . mysqli_connect_error();
 			exit();}
 
-	else {	$sql = "INSERT INTO details (firstname. lastname, email, password, address1, address2, eircode) VALUES(firstname, lastname, email, password, address1, address2, eircode)";}
+	else {	$sql = "INSERT INTO details (firstname, lastname, email, password, address1, address2, eircode) VALUES('$_POST[$firstname]', 'lastname','email','password','address1','address2','eircode')";}
 
 	if (mysqli_query($connection, $sql)){
 			
-			echo("Data added sucesfully");}
+			echo("Data added sucesfully <br/>");}
 
 	else {
-			echo("Unable to add data at this time" . mysqli_errno($connection));}
+			echo("Unable to add data at this time <br/>" . mysqli_errno($connection));}
 
 	mysqli_close($connection);
 ?>
